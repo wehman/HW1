73900aa053d4c154d6b9d0203fb2bc83620f8977
Pat Wehman <patwehman@gmail.com>
2017-01-12 13:09:15 -0500

    Initial commit of my homework.

---
+++ ex2.rb
@@ -0,0 +1,13 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts ""
+# this is a comment.
+puts "I could have code like this."
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+puts "This will run."
